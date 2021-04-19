4888e2753218a366cc20635854a5054c5c86cb8e
miracosmanoglu <mrccc23@gmail.com>
2021-04-19 22:55:16 +0300

    feat: sidebar component added with responsive

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -4,11 +4,13 @@ import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import ProfileBanner from "../../components/ProfileBanner/ProfileBanner";
 import PostCard from "../../components/PostCard/PostCard";
+import SideBar from "../../components/SideBar/SideBar";
 
 function Profile() {
   return (
     <ProfileWrapper>
       <Navbar />
+      <SideBar />
       <ProfileBanner />
       <Footer />
     </ProfileWrapper>
