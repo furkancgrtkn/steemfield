1a397d49d9e710d37b42941222406b40f8bcadac
Furkan Çığırtkan <73472352+furkancgrtkn@users.noreply.github.com>
2021-04-19 21:16:52 +0300

    Feat: Profile modulerize.

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,51 +1,15 @@
-import React, { useState } from "react";
-import {
-  ProfileWrapper,
-  ProfileBannerWrapper,
-  BannerWrapper,
-  ProfileInfos,
-  ProfileInfosTop,
-  UserImage,
-  UserName,
-  UserElips,
-} from "./styled";
-import PostCard from "../../components/PostCard/PostCard";
+import React from "react";
+import { ProfileWrapper } from "./styled";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
-import { LazyLoadImage } from "react-lazy-load-image-component";
+import ProfileBanner from "../../components/ProfileBanner/ProfileBanner";
+import PostCard from "../../components/PostCard/PostCard";
 
 function Profile() {
   return (
     <ProfileWrapper>
       <Navbar />
-      <ProfileBannerWrapper>
-        <BannerWrapper>
-          <LazyLoadImage
-            alt="User Banner"
-            height="100%"
-            effect="opacity"
-            src="https://res.cloudinary.com/dhy7yh4aa/image/upload/v1618847999/web-design-concept-with-drawings_1_mhpj7v.png"
-            width="100%"
-          />
-        </BannerWrapper>
-        <ProfileInfos>
-          <UserImage>
-            <LazyLoadImage
-              alt="User"
-              height="100%"
-              effect="opacity"
-              src="https://res.cloudinary.com/dhy7yh4aa/image/upload/v1618619242/d736a5e43f14ee35df2de3d699f00aa1_ymfhub.jpg"
-              width="100%"
-            />
-          </UserImage>
-          <ProfileInfosTop>
-            <UserName>
-              Pelin <span>Özkan</span>
-            </UserName>
-            <UserElips>(124)</UserElips>
-          </ProfileInfosTop>
-        </ProfileInfos>
-      </ProfileBannerWrapper>
+      <ProfileBanner />
       <Footer />
     </ProfileWrapper>
   );
