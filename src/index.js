236058e6bc59a7abfdb604667b9ae3def48e3ac3
Furkan Çığırtkan <73472352+furkancgrtkn@users.noreply.github.com>
2021-04-23 05:16:49 +0300

    Feat: Profile 4 page completed.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import "swiper/swiper-bundle.css";
+import "react-lazy-load-image-component/src/effects/opacity.css";
 
 import App from "./App";
 ReactDOM.render(<App />, document.getElementById("root"));
